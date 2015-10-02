d665472cb81de4585330d3f1b36b6dfb6b48e625
Calvin0630 <cg7057705@gmail.com>
2015-10-02 18:28:47 -0400

    V 0.0

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public float moveSpeed;
+    Vector3 positiveMovement;
+    public float jumpForce;
+    Vector3 jump;
+    Rigidbody rBody;
+
+	// Use this for initialization
+	void Start () {
+        positiveMovement = new Vector3(moveSpeed, 0, 0);
+        jump = new Vector3(0, jumpForce, 0);
+        rBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rBody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rBody.velocity.y, 0);
+
+	}
+}
